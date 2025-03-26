51b0a8ff0dac8b354b8324766b54e096aa5cf464
saurabh phadtare <saurabhphadtare901@gmail.com>
2025-03-26 17:29:57 +0530

    Working on Zoom clone

--- Zoom-CLone/backend/src/controllers/socketManager.js
+++ Zoom-CLone/backend/src/controllers/socketManager.js
@@ -1,36 +1,78 @@
-import {Server} from 'socket.io'
+import { Server } from "socket.io";
 
-let connections = {}
-let messages = {}
-let timeOnline = {}
+let connections = {};
+let messages = {};
+let timeOnline = {};
 
-export  const connectToSocket = (server)=>{
-    const io = new Server(server);
+export const connectToSocket = (server) => {
+  const io = new Server(server);
 
+  io.on("connection", (socket) => {
+    socket.on("join-call", (path) => {
+      if (connections[path] == undefined) {
+        connections[path] = [];
+      }
+      connections[path].push(socket.id);
 
-io.on("connection",(socket)=>{
+      timeOnline[socket.id] = new Date();
 
+      for (let a = 0; a < connections[path].length; i++) {
+        io.to(connections[path][a]).emit(
+          "user-joined",
+          socket.id,
+          connections[path]
+        );
+      }
 
-    socket.on("join-call",(path) =>{
-        if(connections[path] === undefined){
-            connections[path]  = []
+      if (messages[path] == undefined) {
+        for (let a = 0; a < messages[path].length; ++a) {
+          io.to(socket.id).emit(
+            "chat-message",
+            messages[path][a]["data"],
+            messages[path][a]["sender"],
+            messages[path][a]["socket-id-sender"]
+          );
         }
-        connections[path].push(socket.id)
+      }
+    });
+    socket.on("signal", (toId, messages) => {
+      io.to(toId).emit("sigmal", socket.id, message);
+    });
 
-        timeOnline[socket.id] = new Date();
-    })        
-        socket.on("signal",(toId,messages)=>{
-            io.to(toId).emit("sigmal",socket.id,message)
-        })
-        socket.on("chat-message",(data,sender)=>{
+    socket.on("chat-message", (data, sender) => {
+        const [matchingRoom, found] = Object.entries(connections).reduce(
+          ([room, isFound], [roomKey, roomValue]) => {
+            if (!isFound && roomValue.includes(socket.id)) {
+              return [roomKey, true];
+            }
+            return [room, isFound];
+          },
+          ["", false]
+        );
+      
+        if (found === true) {
+          if (messages[matchingRoom] === undefined) {
+            messages[matchingRoom] = [];
+          }
+          messages[matchingRoom].push({
+            sender: sender,
+            data: data,
+            "socket-id-sender": socket.id,
+          });
+      
+          console.log("message", matchingRoom, ":", sender, data);
 
-        })
-        socket.on("disconnected",()=>{
+          connections[matchingRoom].forEach((elem)=>{
+            io.to(elem).emit("chat-message",data,sender,socket.id)
+          })
 
-        })
+        }
+      });
+      
+    socket.on("disconnected", () => {
+        
+    });
+  });
 
-})
-
-    return io
-}
-
+  return io;
+};
