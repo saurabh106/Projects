3e3dce9711ebb9f79046ef5358f2fc9be54ef6b2
saurabh phadtare <saurabhphadtare901@gmail.com>
2025-04-20 22:08:18 +0530

    Creating new table and convex setup

--- AI-Short-video/ai-video/app/provider.js
+++ AI-Short-video/ai-video/app/provider.js
@@ -8,39 +8,42 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "@/configs/firebaseConfig";
 import { AuthContext } from "./_context/AuthContext";
+import { ConvexProvider, ConvexReactClient, useMutation } from "convex/react";
 
 const Provider = ({ children }) => {
-
-
-  const [user,setUser] = useState()
+  const [user, setUser] = useState();
+  const createUser = useMutation()
+  
+  const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       console.log(user);
-      setUser(user)
+      setUser(user);
     });
     return () => unsubscribe();
   }, []);
 
   return (
     <div>
-    <AuthContext.Provider value={{user}}>
-      <NextThemesProvider
-        attribute="class"
-        defaultTheme="dark"
-        disableTransitionOnChange
-      >
-        {children}
-      </NextThemesProvider>
-      </AuthContext.Provider>
+      <ConvexProvider client={convex}>
+        <AuthContext.Provider value={{ user }}>
+          <NextThemesProvider
+            attribute="class"
+            defaultTheme="dark"
+            disableTransitionOnChange
+          >
+            {children}
+          </NextThemesProvider>
+        </AuthContext.Provider>
+      </ConvexProvider>
     </div>
   );
 };
 
-
-export const useAuthContext = () =>{
-  const context = useContext(AuthContext)
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
   return context;
-}
+};
 
 export default Provider;
---
+++ AI-Short-video/ai-video/convex/users.js
@@ -0,0 +1,39 @@
+//Mutation is like a we want to make a operation like a insert create delete
+
+import { mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+export const CreateNewUser = mutation({
+  args: {
+    name: v.string(),
+    email: v.string(),
+    pictureURL: v.string(),
+  },
+  handler: async (ctx, args) => {
+    //If user already exist
+    const user = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("email"), args.email))
+      .collect();
+
+    if (!user[0]?.email) {
+      //If not , insert new  user
+      const result = await ctx.db.insert("users", {
+        name: args.name,
+        email: args.email,
+        pictureURL: args?.pictureURL,
+        credits: 3,
+      });
+      return result;
+    }
+    return user[0];
+  },
+});
+
+// .collect()
+// This part actually runs the query and gives you back the results as an array (a list).
+// ctx.db.query("users")
+// You're saying:
+// "Hey database, I want to search inside the 'users' collection (like a table of users)."
+// ctx.db is your connection to the database.
+// .query("users") means you're going to search or filter inside the "users" list.
