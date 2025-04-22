7bffb8aae939914ff64911e35f3b18324695a8d5
saurabh phadtare <saurabhphadtare901@gmail.com>
2025-04-22 16:37:12 +0530

    Sidebar completed now working on video screen

--- AI-Short-video/ai-video/app/(main)/create-new-video/_components/Topic.jsx
+++ AI-Short-video/ai-video/app/(main)/create-new-video/_components/Topic.jsx
@@ -1,7 +1,25 @@
 import { Input } from '@/components/ui/input'
-import React from 'react'
+import React, { useState } from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Button } from '@/components/ui/button'
 
+const suggestions = [
+    "Historic Story",
+    "Kids Story",
+    "Movie Stories",
+    "AI Innovations",
+    "Space Mysteries",
+    "Horror Stories",
+    "Mythological Tales",
+    "Tech Breakthroughs",
+    "True Crime Stories",
+    "Fantasy Adventures",
+    "Science Experiements",
+    "Motivational Stories"
+    
+]
 const Topic = () => {
+  const [selectTopic ,setSelectTopic] = useState(false)
   return (
     <div><h2 className='mb-2'>
         Project Title 
@@ -12,6 +30,21 @@ const Topic = () => {
 <div className='mt-5'>
 <h2>Video Topic </h2>
 <p className='text-sm text-gray-600'>Select topic for your video</p>
+
+
+<Tabs defaultValue="suggestion" className="w-full mt-2">
+  <TabsList>
+    <TabsTrigger value="suggestion">Suggestion</TabsTrigger>
+    <TabsTrigger value="your_topic">Your Topic</TabsTrigger>
+  </TabsList>
+  <TabsContent value="suggestion"><div >
+    {suggestions.map((suggestion,index)=>(
+      <Button variant="outline" key={index} className="m-1">{suggestion}</Button>
+    ))}
+  </div></TabsContent>
+  <TabsContent value="your_topic">Change your password here.</TabsContent>
+</Tabs>
+
 </div>
     </div>
   )
