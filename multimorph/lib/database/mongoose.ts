d7f8d4df391dac065072996d7fa02ea5709f1573
saurabh phadtare <saurabhphadtare901@gmail.com>
2025-05-30 14:44:26 +0530

    Added a mongodb setup

---
+++ multimorph/lib/database/mongoose.ts
@@ -0,0 +1,40 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+// Each time a Next.js API route is hit, the code runs fresh (serverless by default).
+// So you need to prevent reconnecting every time, which can lead to performance issues or errors.
+// For this reason we need to use the connection caching pattern
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URL) throw new Error("MONGODB_URL is not defined");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "multimorph",
+      bufferCommands: false,
+    });
+
+    cached.conn = await cached.promise;
+    return cached.conn
+};
