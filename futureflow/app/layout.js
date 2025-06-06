064af6eb295639f43dc40a0848ed7e6d423307d1
saurabh phadtare <saurabhphadtare901@gmail.com>
2025-06-06 20:56:10 +0530

    Working on the landing page

--- futureflow/app/layout.js
+++ futureflow/app/layout.js
@@ -6,45 +6,42 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
 const inter = Inter({
-  subsets: ["latin"]
+  subsets: ["latin"],
 });
 
 export const metadata = {
   title: "FutureFlow – AI planning guide",
   description: "",
-   icons: {
-    icon: '/favicon.png', 
+  icons: {
+    icon: "/favicon.png",
   },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider appearance={{
-      baseTheme: dark,
-    }}>
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className}`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-        {/* header */}
-        <Header/>
-        <main className="min-h-screen">
-          {children}</main>
-          {/* footer */}
-          <footer className="bg-muted/50 py-12">
-            <div className="container mx-auto px-4 text-center text-gray-200">
-              <p>Made by anonymous!</p>
-            </div>
-          </footer>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made by anonymous!</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
--- futureflow/components/hero.jsx
+++ futureflow/components/hero.jsx
@@ -31,7 +31,8 @@ const HeroSection = () => {
     <section className="w-full pt-36 md:pt-48 pb-10">
       <div className="space-y-6 text-center">
         <div className="space-y-6 mx-auto">
-          <h1 className="text-5xl font-extrabold tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-600 pb-2 pr-2 md:text-6xl lg:text-7xl xl:text-8xl">
+          <h1 className="text-5xl font-extrabold tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-blue-500
+           to-purple-600 pb-2 pr-2 md:text-6xl lg:text-7xl xl:text-8xl">
             Your AI Career Coach for
             <br />
             Professional Success
