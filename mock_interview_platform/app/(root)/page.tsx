fea025c17f9900b6971c8c62b8540bc9355ba03f
Saurabh Phadtare <saurabhphadtare901@gmail.com>
2025-09-01 00:39:47 +0530

    Created HomePage

--- mock_interview_platform/app/(root)/page.tsx
+++ mock_interview_platform/app/(root)/page.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import InterviewCard from "@/components/InterviewCard";
+import { Button } from "@/components/ui/button";
+import { dummyInterviews } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Page = () => {
   return (
-    <div>Home </div>
-  )
-}
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>
+            Prepare for Your Next Interview with AI-Powered Simulations &
+            Feedback
+          </h2>
+          <p className="text-lg">
+            Sharpen your skills with real interview questions and receive
+            instant, personalized feedback.
+          </p>
 
-export default Page
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start Now - It's Free</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="AI Robot"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      <section className="flex flex-col items-center text-center mt-20 px-4 max-w-3xl mx-auto">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+          {/* <p>You haven't taken any interviews yet</p> */}
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id}/>
+          ))}
+
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Page;
